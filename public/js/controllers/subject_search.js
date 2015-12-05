72e02e96df3872c96513836723e0ff8d6942325c
kgoins <kgoins@github.com>
2015-12-05 14:09:24 -0500

    checking out master

--- public/js/controllers/subject_search.js
+++ public/js/controllers/subject_search.js
@@ -2,22 +2,22 @@
 
 angular.module('mean').controller('Subject_Search-Controller', ['$scope', 'Authentication', '$state', '$http',
     function ($scope, Authentication, $state, $http) {
-        var results;
 
         // populate results
         $scope.search = function() {
+            console.log($scope.lname);
             $http.get('/subject/search', {
                 params: {
                     sid: $scope.subject_id,
-                    gid: $scope.gid,
                     fname: $scope.fname,
                     lname: $scope.lname
                 }
             })
             .success(function(data) {
-                results = data;
-                console.log(data);
+                $scope.results = data;
             })
-            .error(function() {results = 'There was an error in the search'});
-        } // end search
+            .error(function(err) {
+                $scope.results = 'There was an error in the search';
+            });
+        }; // end search
 }]);
